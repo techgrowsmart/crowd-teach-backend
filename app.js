0663b43a3179b71a0d5b8e6d0c7777cf833ffcba
Saptash Chaubey <145853353+Scoder6@users.noreply.github.com>
2025-12-04 14:13:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1282,7 +1282,7 @@ app.get("/", (req, res) => {
 
 
 const HOST = process.env.HOST
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`🚀 Server running on http://${HOST}:${PORT}`));
 
 
